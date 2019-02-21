f3b4b7e609c98fe5e9899e457061f0942c31750b
michaelscof <pkagrani@gmail.com>
2019-02-21 16:45:37 +0530

    Action type added

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,11 +1,10 @@
 export default function reducers(state, action) {
     if (action.type === 'add') {
-        
-    }
-    console.log(state, action.user);
-    if(action.user)
-        state.users.push(action.user);
-    return{
-        users:state.users
+        console.log(state, action.user);
+        if (action.user)
+            state.users.push(action.user);
+        return {
+            users: state.users
+        }
     }
 }
