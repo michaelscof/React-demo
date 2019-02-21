a4103486a025919d2d08cef4e8f32e558755bb91
michaelscof <pkagrani@gmail.com>
2019-02-21 10:21:56 +0530

    Milestones

--- src/components/searchform.js
+++ src/components/searchform.js
@@ -1,14 +1,18 @@
 import React,{Component} from 'react';
+import Counter from './counter';
 
 export class SearchForm extends Component{
     constructor(){
         super();
         this.state={
             users:[],
-            user:{fName:'',
+            user:{
+            fName:'',
             age:0,
-            gender:''}
+            gender:''
+        }
         };
+        this.sortCounter=true;
         this.updateModel=this.updateModel.bind(this);
     }
     render(){
@@ -18,20 +22,33 @@ export class SearchForm extends Component{
             <input name="age" value={this.state.user.age} onChange={this.updateModel}></input>
             <input type="radio" name="gender" value="male" onChange={this.updateModel}/>male
             <input type="radio" name="gender" value="female" onChange={this.updateModel}/>female
-            <button onClick={(event)=>{
+            <button onClick={()=>{
                 this.state.users.push(this.state.user);
-                this.setState({
-                    users:this.state.users  
-                })
+                this.changeUsersState();
             }}>Search</button>
+            <button onClick={()=>{
+                    this.state.users.sort((user1,user2)=>this.sortCounter?user1.age-user2.age:user2.age-user1.age);
+                    this.sortCounter=!this.sortCounter;
+                    this.changeUsersState();
+            }}>Sort</button>
+            <Counter count={this.state.users.length}></Counter>
             <ol>
                { 
-                this.state.users.map((user)=>
-                <li>{user.fName},{user.age},{user.gender}</li>)
+                this.state.users.map((user,index)=>
+                <li>{user.fName},{user.age},{user.gender}{<button onClick={()=>{
+                    this.state.users.splice(index,1);
+                    this.changeUsersState();
+                }}>Delete</button>}</li>)
                }
             </ol>
         </div>
         );
+    }
+
+    changeUsersState(){
+        this.setState({
+            users:this.state.users
+        })
     }
 
     updateModel(event){
